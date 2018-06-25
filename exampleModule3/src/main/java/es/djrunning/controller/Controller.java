782e6127fc7db2e546982c58819b4d67c18d7c57
aguiu <aguiuperez@gmail.com>
2018-06-25 13:42:27 +0200

    Initial commit

---
+++ exampleModule3/src/main/java/es/djrunning/controller/Controller.java
@@ -0,0 +1,18 @@
+package es.djrunning.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class Controller {
+    private static final String greetings = "Greetings from module 3!";
+
+    @RequestMapping("/")
+    public String index() {
+        return greetings;
+    }
+}
